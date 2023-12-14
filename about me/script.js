5123bfdfe24c8095a0899b4e6eb62ebdb2e1b8c6
Gamerboy76 <kathrynkeys7@gmail.com>
2023-12-14 15:30:25 -0700

    jgirteopgjrteiopgjreiopgrje

---
+++ about me/script.js
@@ -0,0 +1,13 @@
+let peeButton = document.getElementById("pee-button")
+let peeOutput = document.getElementById("pee-output")
+let button = document.getElementById("submit-button")
+console.log(peeButton)
+console.log(peeOutput)
+
+peeOutput.textContent = "pee"
+
+
+
+function feedbackSubmission() {
+    console.log("form submitted")
+}
