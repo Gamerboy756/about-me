47c8d6d63a558c133aa68076b1e8f729b4db130c
Gamerboy76 <kathrynkeys7@gmail.com>
2023-12-21 15:06:06 -0700

    vfegegew

--- about me/script.js
+++ about me/script.js
@@ -17,34 +17,47 @@ function randomlyGenerateText() {
     // console.log(randomText)
     // peeOutput.textContent = randomText
 
+
     let textTypes = ["advice", "date advice", "quote",]
     let textType = getRandomListItem(textTypes)
+
     switch (textType) {
+
         case "advice":
             text = getRandomListItem([
+
                 "don't hang out with bears, they might eat you man",
                 "don't be mean to ai, it might bite you",
                 "oranges are good, you should eat them"
+
             ])
 
+            peeOutput.textContent = text
             return
 
         case "date advice":
             text = getRandomListItem([
+
                 "don't date a serial killer",
                 "don't date chairs",
                 "don't date people who prefer 0.9 lead"
+
             ])
+
             peeOutput.textContent = text
             return
 
         case "quote":
+
             text = getRandomListItem([
+
                 "'PIGEONS ARE LIARS!!!!' -my brother",
                 "'wherever you go, there you are.' -my mom",
                 "'i don't know' -mataya",
                 "'uhhh idk' -my sister"
+
             ])
+
             peeOutput.textContent = text
             return
     }
